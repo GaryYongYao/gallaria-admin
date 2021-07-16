33eac15bfca8adb94179c1cc21abf0feb7f22f58
GaryYongYao <doppelganger_tianjay@hotmail.com>
2021-07-17 02:59:43 +0800

    Update Faq.js

--- client/src/modules/help/component/Faq.js
+++ client/src/modules/help/component/Faq.js
@@ -14,6 +14,12 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
   },
+  accordionBreak: {
+    marginBottom: 10
+  },
+  sectionBreak: {
+    marginBottom: 20
+  },
   heading: {
     fontSize: theme.typography.pxToRem(15),
     flexBasis: '33.33%',
@@ -22,6 +28,9 @@ const useStyles = makeStyles((theme) => ({
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
+  },
+  paraList: {
+    display: 'block'
   },
 }))
 
@@ -36,7 +45,8 @@ function HelpSection() {
   return (
     <Grid item xs={12} md={9}>
       <div className={classes.root}>
-        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <Typography variant="h5" className={classes.sectionBreak}>Help</Typography>
+        <Accordion className={classes.accordionBreak} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1bh-content"
@@ -50,7 +60,7 @@ function HelpSection() {
             </Typography>
           </AccordionDetails>
         </Accordion>
-        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <Accordion className={classes.sectionBreak} expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel2bh-content"
@@ -64,6 +74,145 @@ function HelpSection() {
                 Follow this link to change bank or currencies for the Stripe payout.
               </Typography>
             </Link>
+          </AccordionDetails>
+        </Accordion>
+        <Typography variant="h5" className={classes.sectionBreak}>Quotas and Cost after quota</Typography>
+        <Accordion className={classes.sectionBreak} expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <Typography className={classes.heading}>Netlify (Website Hosting)</Typography>
+          </AccordionSummary>
+          <AccordionDetails className={classes.paraList}>
+            <Typography paragraph>
+              <b>Build Minute</b>
+            </Typography>
+            <Typography paragraph>
+              Quota: 300 minutes / month
+            </Typography>
+            <Typography paragraph>
+              Quota Refresh: 24th every month
+            </Typography>
+            <Typography paragraph>
+              Overquota cost: $7 / 500 minutes
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className={classes.sectionBreak} expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <Typography className={classes.heading}>Google Maps API</Typography>
+          </AccordionSummary>
+          <AccordionDetails className={classes.paraList}>
+            <Typography paragraph>
+              <b>Monthly Google Quotas</b>
+            </Typography>
+            <Typography paragraph>
+              $200 per month
+            </Typography>
+            <Typography paragraph>
+              Refresh on 1st of every month
+            </Typography>
+            <Typography paragraph>
+              <b>Geocoding (only backend)</b>
+            </Typography>
+            <Typography paragraph>
+              Cost: $0.005 per request
+              <br />
+              <Typography variant="caption">
+                *Only will be billed if the request is over $200
+              </Typography>
+            </Typography>
+            <Typography paragraph>
+              <b>Google Maps API</b>
+            </Typography>
+            <Typography paragraph>
+              Cost: $0.007 per request
+              <br />
+              <Typography variant="caption">
+                *Only will be billed if the request is over $200
+              </Typography>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className={classes.sectionBreak} expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <Typography className={classes.heading}>AWS S3 (Media Storage)</Typography>
+          </AccordionSummary>
+          <AccordionDetails className={classes.paraList}>
+            <Typography paragraph>
+              <b>Monthly Data Transfer Quota</b>
+            </Typography>
+            <Typography paragraph>
+              Quota: 15GB / month
+            </Typography>
+            <Typography paragraph>
+              Quota Refresh: 1st every month
+            </Typography>
+            <Typography paragraph>
+              Overquota cost: $0.114 / GB
+            </Typography>
+            <Typography paragraph>
+              <b>Storage Usage</b>
+            </Typography>
+            <Typography paragraph>
+              Storage Quota: 15GB
+            </Typography>
+            <Typography paragraph>
+              Storage Request: 20,000 Request / month
+            </Typography>
+            <Typography paragraph>
+              Quota Refresh: 1st every month
+            </Typography>
+            <Typography paragraph>
+              Overquota cost: $0.0044 per 10,000 requests
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className={classes.sectionBreak} expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <Typography className={classes.heading}>MongoDB Database</Typography>
+          </AccordionSummary>
+          <AccordionDetails className={classes.paraList}>
+            <Typography paragraph>
+              <b>Database</b>
+            </Typography>
+            <Typography paragraph>
+              Quota: 512MB
+            </Typography>
+            <Typography paragraph>
+              Impossible to overquota, unless you have millions of products
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className={classes.sectionBreak} expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+          >
+            <Typography className={classes.heading}>Heroku (Backend Hosting)</Typography>
+          </AccordionSummary>
+          <AccordionDetails className={classes.paraList}>
+            <Typography paragraph>
+              <b>Hosting</b>
+            </Typography>
+            <Typography paragraph>
+              Included with maintenance
+            </Typography>
           </AccordionDetails>
         </Accordion>
       </div>
