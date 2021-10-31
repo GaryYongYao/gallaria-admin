870a89e091685c6411ecc40b19071e6c6b4c5f73
GaryYongYao <doppelganger_tianjay@hotmail.com>
2021-10-31 18:03:00 +0800

    Update feature.js

--- graphql/resolvers/handlerGenerators/feature.js
+++ graphql/resolvers/handlerGenerators/feature.js
@@ -5,8 +5,9 @@ const Feature = require('../../../models/feature')
 async function getFeaturedAdmin() {
   try {
     const feature = await Feature.find().populate(['product']).limit(11)
+    const filtered = feature.filter(({ product }) => (product.code))
 
-    return feature
+    return filtered
   }
   catch(err) {
     throw err
@@ -16,8 +17,9 @@ async function getFeaturedAdmin() {
 async function getFeatured() {
   try {
     const features = await Feature.find().populate(['product']).limit(11)
+    const filtered = features.filter(({ product }) => (product.code))
 
-    return features.map(({ product }) => ({
+    return filtered.map(({ product }) => ({
       code: (product || {}).code,
       name: (product || {}).name,
       featureImage: (product || {}).featureImage
@@ -31,8 +33,9 @@ async function getFeatured() {
 async function getCarousel() {
   try {
     const features = await Feature.find().populate(['product']).limit(5)
+    const filtered = features.filter(({ product }) => (product.code))
 
-    return features.map(({ product }) => ({
+    return filtered.map(({ product }) => ({
       code: (product || {}).code,
       name: (product || {}).name,
       featureImage: (product || {}).featureImage
